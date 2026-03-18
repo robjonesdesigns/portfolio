a27b092aaf28e051afbd344963e99949fbac4d40
Rob Jones <robjonesdesigns@gmail.com>
2026-03-18 12:57:25 -0400

    Fix Sitemap: add LazyMotion wrapper so Navbar animations work

--- src/components/pages/SitemapPage.jsx
+++ src/components/pages/SitemapPage.jsx
@@ -1,3 +1,4 @@
+import { LazyMotion, domAnimation, MotionConfig } from 'framer-motion'
 import { useTheme } from '../../hooks/useTheme'
 import Navbar from '../layout/Navbar'
 import Footer from '../layout/Footer'
@@ -6,6 +7,8 @@ export default function SitemapPage() {
   const { theme, toggle } = useTheme()
 
   return (
+    <LazyMotion features={domAnimation}>
+      <MotionConfig reducedMotion="user">
     <div className="noise">
       <a href="#main" className="skip-link">Skip to main content</a>
       <Navbar theme={theme} toggleTheme={toggle} />
@@ -37,5 +40,7 @@ export default function SitemapPage() {
       </main>
       <Footer />
     </div>
+      </MotionConfig>
+    </LazyMotion>
   )
 }
